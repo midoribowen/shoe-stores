504fa6b5faa1b8c5d36a6796bb4c653c634a783c
Midori Bowen <midori.p.bowen@gmail.com>
2016-01-29 15:33:59 -0800

    Pass integration test to successfully add a store.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,11 +13,41 @@ public class App {
 
     get("/", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
-      model.put("stores", Store.all());
-      model.put("brands", Brand.all());
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    get("/stores", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("stores", Store.all());
+      model.put("template", "templates/stores.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/stores", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Store store = new Store(request.queryParams("store-name"));
+      store.save();
+      model.put("store", store);
+      response.redirect("/stores");
+      return null;
+    });
+
+    get("/brands", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("brands", Brand.all());
+      model.put("template", "templates/brands.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/brands", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Brand brand = new Brand(request.queryParams("brand-name"));
+      brand.save();
+      model.put("brand", brand);
+      response.redirect("/brands");
+      return null;
+    });
 
   }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -29,21 +29,45 @@ public class AppTest extends FluentTest {
   }
 
   @Test
-  public void displayAllStoresAndBrands() {
-    Store firstStore = new Store("Foot Locker");
-    Store secondStore = new Store("Woodstock Shoes");
-    firstStore.save();
-    secondStore.save();
-    Brand firstBrand = new Brand("Nike");
-    Brand secondBrand = new Brand("Adidas");
-    Brand thirdBrand = new Brand("Adidas");
-    firstBrand.save();
-    secondBrand.save();
-    thirdBrand.save();
+  public void storeAddedSuccessfully() {
     goTo("http://localhost:4567/");
+    fill("#store-name").with("Woodstock Shoes");
+    submit(".new-store");
+    goTo("http://localhost:4567/stores");
     assertThat(pageSource()).contains("Woodstock");
-    assertEquals(Store.all().size(), 2);
-    assertEquals(Brand.all().size(), 3);
   }
+
+  // @Test
+  // public void brandAddedSuccessfully() {
+  //   goTo("http://localhost:4567/");
+  //   fill("#brand-name").with("Adidas");
+  //   submit(".new-brand");
+  //   click("a", withText("Brands"));
+  //   assertThat(pageSource()).contains("Adidas");
+  // }
+  //
+  //   @Test
+  //   public void displayAllStores() {
+  //     Store firstStore = new Store("Foot Locker");
+  //     Store secondStore = new Store("Woodstock Shoes");
+  //     firstStore.save();
+  //     secondStore.save();
+  //     goTo("http://localhost:4567/stores");
+  //     assertThat(pageSource()).contains("Woodstock");
+  //     assertEquals(Store.all().size(), 2);
+  //   }
+  //
+  //   @Test
+  //   public void displayAllBrands() {
+  //     Brand firstBrand = new Brand("Nike");
+  //     Brand secondBrand = new Brand("Adidas");
+  //     Brand thirdBrand = new Brand("Sketchers");
+  //     firstBrand.save();
+  //     secondBrand.save();
+  //     thirdBrand.save();
+  //     goTo("http://localhost:4567/brands");
+  //     assertThat(pageSource()).contains("Adidas");
+  //     assertEquals(Brand.all().size(), 3);
+  //   }
 
 }
