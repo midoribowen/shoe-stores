ce83c666202b6e0f40d07d0b3b68f3dac962a923
Midori Bowen <midori.p.bowen@gmail.com>
2016-01-29 14:15:53 -0800

    Pass integration test to display all stores and brands in the database on the home page.

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.fluentlenium.core.filter.FilterConstructor.*;
+import static org.junit.Assert.*;
 
 public class AppTest extends FluentTest {
   public WebDriver webDriver = new HtmlUnitDriver();
@@ -23,8 +24,26 @@ public class AppTest extends FluentTest {
 
   @Test
   public void rootTest() {
-    goTo("http://localhost:4567");
+    goTo("http://localhost:4567/");
     assertThat(pageSource()).contains("Local Shoe Stores");
+  }
+
+  @Test
+  public void displayAllStoresAndBrands() {
+    Store firstStore = new Store("Foot Locker");
+    Store secondStore = new Store("Woodstock Shoes");
+    firstStore.save();
+    secondStore.save();
+    Brand firstBrand = new Brand("Nike");
+    Brand secondBrand = new Brand("Adidas");
+    Brand thirdBrand = new Brand("Adidas");
+    firstBrand.save();
+    secondBrand.save();
+    thirdBrand.save();
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("Woodstock");
+    assertEquals(Store.all().size(), 2);
+    assertEquals(Brand.all().size(), 3);
   }
 
 }
